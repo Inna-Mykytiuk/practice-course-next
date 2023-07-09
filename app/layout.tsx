99e176e7689eebb701ed1cbda9a2b2adc705de16
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-07-10 00:01:06 +0300

    components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-// import { Header } from "@components/Header"
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import { Navigation } from "./Navigation";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
         <div className="nav">
-          <Link href="/" className="link">
-            Home
-          </Link>
-          <Link href="/blog" className="link">
-            Blog
-          </Link>
-          <Link href="/about" className="link">
-            About
-          </Link>
+          <Navigation navLinks={navItems} />
         </div>
       </div>
     </header>
