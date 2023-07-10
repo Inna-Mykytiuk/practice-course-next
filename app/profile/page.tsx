27eafb48b65ddfd48382f3e55642c4ccf5f991f0
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-07-10 18:35:33 +0300

    styles

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,10 +7,16 @@ export default async function Profile() {
   const session = await getServerSession(authConfig);
 
   return (
-    <div>
-      <h1>Profile of {session?.user?.name}</h1>
+    <div className="profile-block">
+      <h1>{session?.user?.name}</h1>
       {session?.user?.image && (
-        <Image src={session.user.image} alt="" width={100} height={100} />
+        <Image
+          src={session.user.image}
+          alt=""
+          width={100}
+          height={100}
+          className="img"
+        />
       )}
     </div>
   );
