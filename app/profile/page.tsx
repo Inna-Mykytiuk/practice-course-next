0d09170dfb5507020de1a1327088a60e7297fe13
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-07-10 22:11:34 +0300

    upd

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,38 +1,23 @@
-/* eslint-disable @next/next/no-img-element */
-// import { authConfig } from '@/configs/auth';
-// import { getServerSession } from 'next-auth/next';
-
-// import Image from 'next/image';
-
-// export default async function Profile() {
-//   const session = await getServerSession(authConfig);
-
-//   return (
-//     <div className="profile-block">
-//       <h1>{session?.user?.name}</h1>
-//       {session?.user?.image && (
-//         <Image
-//           src={session.user.image}
-//           alt="avatar"
-//           width={100}
-//           height={100}
-//           className="img"
-//         />
-//       )}
-//     </div>
-//   );
-// }
-
 import { authConfig } from '@/configs/auth';
 import { getServerSession } from 'next-auth/next';
+
+import Image from 'next/image';
 
 export default async function Profile() {
   const session = await getServerSession(authConfig);
 
   return (
-    <div>
-      <h1>Profile of {session?.user?.name}</h1>
-      {session?.user?.image && <img src={session.user.image} alt="" />}
+    <div className="profile-block">
+      <h1>{session?.user?.name}</h1>
+      {session?.user?.image && (
+        <Image
+          src={session.user.image}
+          alt="avatar"
+          width={100}
+          height={100}
+          className="img"
+        />
+      )}
     </div>
   );
 }
