66edeceb82cabbb835c187daf6a290e827fd0391
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-07-11 22:27:34 +0300

    upd

--- components/GoogleButton.tsx
+++ components/GoogleButton.tsx
@@ -1,14 +1,16 @@
 'use client';
+// require('dotenv').config();
 
 import { signIn } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
 
 const GoogleButton = () => {
   const searchParams = useSearchParams();
+  // const { NEXT_BASE_URL } = process.env;
 
   const callbackUrl =
     searchParams.get('callbackUrl') ||
-    'https://practice-course-next.vercel.app/profile';
+    `https://practice-course-next.vercel.app/profile/profile`;
 
   return (
     <button
